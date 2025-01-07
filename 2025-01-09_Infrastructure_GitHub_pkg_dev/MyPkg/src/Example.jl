cc3b1ce6e412bf83042382641efe3a7d747226c8
Pascal Aellig <93140290+aelligp@users.noreply.github.com>
2025-01-07 20:41:46 +0100

    2025-01-09 - Julia Development with Github (#9)
    * add material
    
    * Create assets
    
    * delete file
    
    * add missing png file
    
    * add missing link

---
+++ 2025-01-09_Infrastructure_GitHub_pkg_dev/MyPkg/src/Example.jl
@@ -0,0 +1,18 @@
+# Here we can add functions and develop the code
+
+"""
+    foo(x)
+I am a documentation string for the function foo.
+This function squared the input variable x.
+"""
+function foo(x)
+    return x^2
+end
+
+"""
+    foo2(x)
+I am another documentation, this time for the function foo2.
+"""
+function foo2(x)
+    return x^3
+end
